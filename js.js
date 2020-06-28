b5609bc5033ec8534687fb85772c9cb5126239fb
mabrouk22 <53973845+mabrouk22@users.noreply.github.com>
2020-06-28 11:49:43 +0200

    Add files via upload

---
+++ js.js
@@ -0,0 +1,24 @@
+window.cookieconsent.initialise({
+  &quot;palette&quot;: {
+    &quot;popup&quot;: {
+      &quot;background&quot;: &quot;#252e39&quot;
+    },
+    &quot;button&quot;: {
+      &quot;background&quot;: &quot;#14a7d0&quot;
+    }
+  },
+  &quot;theme&quot;: &quot;classic&quot;,
+  &quot;position&quot;: &quot;bottom-left&quot;,
+  &quot;content&quot;: {
+&quot;message&quot;: &quot;ÌŒ“&#1616;&#1617;‰ „Êﬁ⁄ «·ÊÌ» Â–« «·»Ì«‰«  „‰ ﬁ»Ì· „·›«   ⁄—Ì› «·«— »«ÿ · „ﬂÌ‰ ÊŸ«∆› «·„Êﬁ⁄ «·„Â„… »„« ›Ì –·ﬂ «· Õ·Ì·«  Ê«·«” Âœ«› Ê«· Œ’Ì’.&quot;,
+&quot;dismiss&quot;: &quot;„Ê«›ﬁ&quot;,
+&quot;link&quot;: &quot;ﬁ—«¡… «·„“Ìœ&quot;,
+&quot;href&quot;: &quot;https://www.afkariik.com/p/blog-page_37.html&quot;}});
+
+///////////////
+
+ var block = [&#39;addmefast.com&#39;, &#39;alexaboostup.com&#39;, &#39;lifehac?er.com&#39;, &#39;hitleap.com&#39;,&#39;siteweb.com&#39;];
+    for (var b = block.length; b--;) {
+    if (document.referrer.match(block[b]))
+    window.location = &quot;https://www.google.com/&quot;;
+    }
